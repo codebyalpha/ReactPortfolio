613e9138b2f5497e88a6d99fcd241a3679a835de
sharad choudhary <kumarsaini1993@gmail.com>
2023-04-22 01:53:18 +0530

    project design updated

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,37 +1,25 @@
 import React from "react";
 import "./Project.css";
-import Clock from "../projects/Clock";
+import Clock from "../projects/Clock/Clock";
 import { Col, Container, Row } from "react-bootstrap";
-import clockimg from "../projects/clock.png";
+import clockimg from "../projects/Clock/clock.png";
 
 function Project() {
   return (
     <Container className="projectcontainer">
-      <Row className="projectrow">
-        <Col>
-          <img src={clockimg} alt="" />
-        </Col>
-        <Col>a</Col>
-      </Row>
-      <Row className="projectrow">
-        <Col>
-          <img src={clockimg} alt="" />
-        </Col>
-        <Col>a</Col>
-      </Row>
-      <Row className="projectrow">
-        <Col>
-          <img src={clockimg} alt="" />
-        </Col>
-        <Col>a</Col>
+      <Row className="projectcont">
+        <Row className="projectrow">
+          <Col>
+            <img src={clockimg} alt="" />
+          </Col>
+          <Col className="projectintro">
+            <li>React</li>
+            <li>CSS</li>
+            <li>TypeScript</li>
+          </Col>
+        </Row>
       </Row>
     </Container>
-    // <Container className="projectcontainer">
-    //   <Row className="projectrow">
-    //     <Col></Col>
-    //     <Col className="projectdesc">a</Col>
-    //   </Row>
-    // </Container>
   );
 }
 
